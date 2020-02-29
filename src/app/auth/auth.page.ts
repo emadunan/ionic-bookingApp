96f7512df403861bb6fa8d29e60813918c790329
emadunan <53171704+emadunan@users.noreply.github.com>
2020-02-29 16:16:35 +0200

    commit 4

--- src/app/auth/auth.page.ts
+++ src/app/auth/auth.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { AuthService } from './auth.service';
 import { Router } from '@angular/router';
 import { LoadingController } from '@ionic/angular';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-auth',
@@ -10,6 +11,7 @@ import { LoadingController } from '@ionic/angular';
 })
 export class AuthPage implements OnInit {
   // isLoading = false;
+  isLogin = true;
 
   constructor(
     private authService: AuthService,
@@ -33,6 +35,25 @@ export class AuthPage implements OnInit {
         this.router.navigateByUrl(`/places/tabs/discover`);
       }, 2000);
     });
+  }
+
+  onSubmit(form: NgForm) {
+    if (!form.valid) {
+      return;
+    }
+    const email = form.value.email;
+    const password = form.value.password;
+    console.log(email, password);
+
+    if (this.isLogin) {
+      // send request to login servers!
+    } else {
+      // send request to signup servers!
+    }
+  }
+
+  onSwitch() {
+    this.isLogin = !this.isLogin;
   }
 
 }
